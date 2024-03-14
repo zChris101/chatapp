6f109e853a7a849510a6f8360a99679b9ac41b70
zChris101 <chrstnmusico@gmail.com>
2024-03-14 15:13:20 +0800

    02:58:31 => added status on avatar static

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -28,6 +28,22 @@ const Avatar = ({ user }: AvatarProps) => {
           fill
         />
       </div>
+      <span
+        className="
+         absolute
+         block
+         rounded-full
+         bg-green-500
+         ring-2
+         ring-white
+         top-0
+         right-0
+         h-2
+         w-2
+         md:h-3
+         md:w-3
+        "
+      />
     </div>
   );
 };
